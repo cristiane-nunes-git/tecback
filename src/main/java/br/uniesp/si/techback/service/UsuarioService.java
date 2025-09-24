b49ded9814ca6f95cef0af6380c5e3af079a23d6
cristiane nunes <165221665+cristiane-nunes-git@users.noreply.github.com>
2025-09-23 23:04:10 -0300

    Criando endpoints para Usuario, Genero,Favoritos,Planos,Endereco

---
+++ src/main/java/br/uniesp/si/techback/service/UsuarioService.java
@@ -0,0 +1,32 @@
+package br.uniesp.si.techback.service;
+
+import br.uniesp.si.techback.model.Usuario;
+import br.uniesp.si.techback.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscarPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deletar(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
